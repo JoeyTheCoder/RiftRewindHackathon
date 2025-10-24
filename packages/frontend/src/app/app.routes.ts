0dcff18a45f29396b6552432c02cb77bbaf77119
JoeyTheCoder <joel.sahli@outlook.com>
2025-10-24 13:36:30 +0200

    complete reset of project

--- packages/frontend/src/app/app.routes.ts
+++ packages/frontend/src/app/app.routes.ts
@@ -1,14 +1,10 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from '../home/home.component';
-import { SummonerProfileComponent } from './summoner-profile/summoner-profile.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-    {
-        path: '',
-        component: HomeComponent
-    },
-    {
-        path: 'summoner/:region/:gameName/:tagLine',
-        component: SummonerProfileComponent
-    }
+  {
+    path: '',
+    component: HomeComponent,
+    pathMatch: 'full'
+  }
 ];
